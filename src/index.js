f69dec5d873acf76f52d3959974d76a603bbdcf2
Physcript <Physcript@gmail.com>
2021-10-25 20:37:07 +0800

    fixing cors

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import { getMainDefinition } from '@apollo/client/utilities'
 
 const uploadLink = new createUploadLink({
   uri: 'https://fast-plains-63623.herokuapp.com/graphql',
-  credentials: 'include'
+  credentials: 'same-origin'
 })
 
 const authLink = setContext( (_,{headers} ) => {
