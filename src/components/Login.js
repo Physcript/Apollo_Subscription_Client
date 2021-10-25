074e983781ae73cb0fff367a725d239b159da28d
Physcript <Physcript@gmail.com>
2021-10-26 01:18:25 +0800

    fixing cors

--- src/components/Login.js
+++ src/components/Login.js
@@ -22,7 +22,7 @@ const Login = () => {
 		onCompleted: e => {
 			setLoginSyntax({})
 			Cookies.set('token',e.loginUser , {  expires: 7 ,secure: true })
-			localStorage.set('token',e.loginUser)
+			localStorage.setItem('token',e.loginUser)
 			history.push({
 				pathname: '/home',
 				state: { auth: true }
